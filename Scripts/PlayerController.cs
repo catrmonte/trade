b774c5ab0a4a5384b51489a9048ec0d443839948
catrmonte <59862274+catrmonte@users.noreply.github.com>
2020-11-24 12:27:13 -0700

    Add files via upload

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rig;
+
+    private float startTime;
+    private float timeTaken;
+
+    private int score;
+    public int maxCollectables = 9;
+
+    private bool isPlaying;
+
+    public GameObject playButton;
+    public TextMeshProUGUI curScoreText;
+    public TextMeshProUGUI curTimeText;
+
+    void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (!isPlaying)
+            return;
+
+        float x = Input.GetAxis("Horizontal") * speed;
+        float z = Input.GetAxis("Vertical") * speed;
+
+        rig.velocity = new Vector3(x, rig.velocity.y, z);
+
+        timeTaken = Time.time - startTime;
+
+        if (timeTaken > 10)
+        {
+            End();
+        }
+
+        curTimeText.text = (10 - timeTaken).ToString("F2");
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collectable"))
+        {
+            Destroy(other.gameObject);
+
+            score += 10;
+
+            curScoreText.text = score.ToString();
+
+            pointSpawner.instance.SpawnOrb();
+        }
+    }
+
+    public void Begin()
+    {
+        startTime = Time.time;
+        isPlaying = true;
+
+        playButton.SetActive(false);
+    }
+
+    void End()
+    {
+        timeTaken = Time.time - startTime;
+        isPlaying = false;
+
+        playButton.SetActive(true);
+        Leaderboard.instance.SetLeaderboardEntry(-(score+1000));
+    }
+}
---
+++ Scripts/pointSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pointSpawner : MonoBehaviour
+{
+    public GameObject[] spawnDrops;
+    private int spawnCounter = 1;
+    public GameObject orbPrefab;
+
+    public static pointSpawner instance;
+
+    void Awake() { instance = this; }
+
+
+    public void SpawnOrb()
+    {
+        if (spawnCounter % 5 == 0)
+        {
+            // Spawn High Point one
+        }
+        else
+        {
+            // Spawn Regular one
+            Instantiate(orbPrefab, spawnDrops[spawnCounter-1].transform.position, Quaternion.identity);
+        }
+        
+        // Check if spawn counter needs to wrap around
+        if (spawnCounter == 10)
+        {
+            spawnCounter = 1;
+        }
+        else
+        {
+            spawnCounter++;
+        }
+
+    }
+}
